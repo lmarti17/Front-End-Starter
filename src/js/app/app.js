d098d20d6bcbb883eeb8256a6b80fb6a23402417
MachuPichu <lucas.martin.78@free.fr>
2017-05-28 20:14:13 +0200

    init real version

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -4,4 +4,49 @@ domready(() =>  {
 
 	console.log('Hi there ! I\'m a 4th year french student @HETIC, and currently looking for a 6-month internship in a creative digital agency.');
 
+
+	// GALLERY PREVIEW-APP
+
+	document.getElementById('preview-app').addEventListener('click', function(e){
+
+		e.preventDefault();
+
+		$.fancybox.open([
+			{
+				src  : 'assets/img/preview-app_01.jpg',
+				opts : {
+					caption : 'Preview App - Imac'
+				}
+			},
+			{
+				src  : 'assets/img/preview-app_02.jpg',
+				opts : {
+					caption : 'Preview App - Macbook'
+				}
+			},
+			{
+				src  : 'assets/img/preview-app_03.jpg',
+				opts : {
+					caption : 'Preview App - Iphone'
+				}
+			},
+			{
+				src  : 'assets/img/preview-app_04.jpg',
+				opts : {
+					caption : 'Preview App - Iphone'
+				}
+			},
+			{
+				src  : 'assets/img/preview-app_05.jpg',
+				opts : {
+					caption : 'Preview App - Iphone'
+				}
+			}
+		], {
+			loop : true
+		});
+
+
+	});
+
 })
